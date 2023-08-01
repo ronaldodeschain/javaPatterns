c232cbf5442d48e85fe1a05d50d1255178564c27
ronaldodeschain <lotushall@gmail.com>
2023-08-01 11:09:49 -0300

    estudo de decorator

--- src/TestesImpostos.java
+++ src/TestesImpostos.java
@@ -1,6 +1,7 @@
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
@@ -10,7 +11,8 @@ public class TestesImpostos {
 		
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		System.out.println(calculadora.calcular(orcamento,new ISS()));
+		//iss ou icms ou outros impostos devem ser colocados nos parametros do calcular
+		System.out.println(calculadora.calcular(orcamento,new ICMS(null)));
 
 	}
 
--- src/br/com/alura/loja/imposto/Imposto.java
+++ src/br/com/alura/loja/imposto/Imposto.java
@@ -4,7 +4,21 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public interface Imposto {
+public abstract class Imposto {
 
-	BigDecimal calcular(Orcamento orcamento);
+	private Imposto outro;
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento); 
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+		//se tem outro imposto faz o calculo antes de ir para o return
+		if(outro != null) {
+			valorDoOutroImposto = outro.realizarCalculo(orcamento);
+		}
+		return valorImposto.add(valorDoOutroImposto);
+	}
 }
